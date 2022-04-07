2e500865c600823cfef369b6c9511269ada5d82d
DianYordanov <dian.yordanov.z@gmail.com>
2022-04-07 19:49:00 +0300

    save

---
+++ src/main/java/com/cvetnica/cvetnicaserver/CvetnicaserverApplication.java
@@ -0,0 +1,13 @@
+package com.cvetnica.cvetnicaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvetnicaserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvetnicaserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cvetnica/cvetnicaserver/CvetnicaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cvetnica.cvetnicaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvetnicaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
